e1243fa99a07849190a244488ba8c0691ed2b3d7
Zhe Wang <zhe.wang888@gmail.com>
2018-01-08 00:03:07 -0800

    add file logging
    Signed-off-by: Zhe Wang <zhe.wang888@gmail.com>

--- engine.py
+++ engine.py
@@ -3,12 +3,11 @@ from binance.enums import *
 import time
 import HFT_algo
 
-ddd = [0,1,2,3,4,5,6,7,8,9]
-
 #Enable Trading
 TradingEnabled = True
 FastFill = False
 useRSI = False
+printOut = True
 
 #Exchanges
 sym = 'BNBETH'
@@ -45,6 +44,11 @@ input_str = input('Enable RSI (Y/N), default no?:')
 if input_str == 'Y':
     useRSI = True
 
+input_str = input('Slience Mode (Y/N), default no?:')
+if input_str == 'Y':
+    printOut = False
+
+
 fee = 0.0005
 limit = False
 
@@ -55,6 +59,7 @@ f = open("API.txt", "r")
 lines = f.read().splitlines()
 api_key = lines[0].split(" ")[2]
 secret = lines[1].split(" ")[2]
+f.close()
 
 #Open client
 client = Client(api_key,secret)
@@ -75,7 +80,7 @@ def Perform(action,price,quan):
         actionStr = "BUY"
 
     if TradingEnabled == False:
-        print(actionStr + "@" + price)
+        LOG(actionStr + "@" + price)
         return
 
     else:
@@ -84,7 +89,7 @@ def Perform(action,price,quan):
                 side=side,
                 type = 'MARKET',
                 quantity=quan)
-        #print("Made an order:" + actionStr + "@" + str(order['price']) +" x" + str(quan))
+        #LOG("Made an order:" + actionStr + "@" + str(order['price']) +" x" + str(quan))
         #print (order)
         return order
 
@@ -97,6 +102,12 @@ def get_per_min_history(history):
         per_min_hist[int(i/samples_per_min)]+= float(history[i])/samples_per_min;
         i+=1;
     return per_min_hist;
+
+def LOG(s):
+    if(printOut):
+        print(s)
+    with open ("LOG_"+str(StartTime)+".txt","a") as file:
+        file.write(s+'\n')
     
 
 accProfit  = 0
@@ -106,101 +117,107 @@ history = []
 orders = client.get_my_trades(symbol=sym,limit = 1)
 cost = orders[0]['price']
 lastAction = 1
-lastBuyTs = time.clock()
-lastSellTs = lastBuyTs
 
-print (time_now() + "HFT BOT Starting now for " + sym)
-print ("Amount = " + str(amount) +" SamplingRate = " + str(samplingRate) + " Sample Avg window: "+ str(samples_per_min))
-print ("TRADING:" + str(TradingEnabled))
+StartTime = time.clock()
+lastBuyTs = StartTime
+lastSellTs = lastBuyTs
+currTs = StartTime
+profit_if_no_sell = 0
+
+LOG(time_now() + "HFT BOT Starting now for " + sym)
+LOG("Amount = " + str(amount) +" SamplingRate = " + str(samplingRate) + " Sample Avg window: "+ str(samples_per_min))
+LOG("TRADING:" + str(TradingEnabled))
 
 if (FastFill):
-    print("FastFill Enabled... starting filling with faster sampling rate")
+    LOG("FastFill Enabled... starting filling with faster sampling rate")
 
     originalSR = samplingRate
     #use 5sec sample as a min
     samplingRate = 0.075
 
-while(1):
-    run = False
-    
-    #GET DATA
-    #1. price
-    trades = client.get_recent_trades(symbol=sym,limit = 1)
-    price = trades[0]['price']
-    #print(time_now()+price+"   TS:"+str(trades[0]['time']))
-
-    #2 order depth
-    asks = []
-    bids = []
-    if 0:
-        depth = client.get_order_book(symbol=sym)
-        asks = depth['asks']
-        bids = depth['bids']
-    
-    #3. history for MACD, RSI
-    history.append(price)
-    if len(history) > MAX_HIST:
-        history = history[1:MAX_HIST+1]
-        per_min_history = get_per_min_history(history);
-        run = True
-    elif len(history)%(int(MAX_HIST/20)) == 0:
-        print (time_now() + str(int(len(history)/(int(MAX_HIST/20)))*5) + "% history filled")
-
-    if FastFill == True and len(history) == MAX_HIST:
-        samplingRate = originalSR
-        FastFill = False
-        print("FastFill Completed, now samping rate = " + str(samplingRate))
-        print("-----------------------------------------------------------")
-
-    if(run):
-        #Call HFT module to decide
-        currTs = time.clock()
-        action = HFT_algo.HFT(cost, lastBuyTs, currTs, price, fee, per_min_history, asks, bids, lastAction, useRSI)
-
-        #Perform action
-        if (action > 0 and action != lastAction):
-            order = Perform(action,price,amount)
-            lastAction = action
+try:
+    while(1):
+        run = False
         
-            if action == 2:
-                if TradingEnabled:
-                    if order != None:
-                        if order['status']  != 'FILLED':
-                            sleep(0.1)
-                    orders = client.get_my_trades(symbol=sym,limit = 1)
-                    cost = orders[0]['price']
-                else:
-                    cost = price
+        #GET DATA
+        #1. price
+        trades = client.get_recent_trades(symbol=sym,limit = 1)
+        price = trades[0]['price']
+        #LOG(time_now()+price+"   TS:"+str(trades[0]['time']))
 
-                #add fees to price
-                cost = str((1+fee) * float(cost))
-                print("Bought@" + str(cost) +" x" + str(amount))
-                lastBuyTs = time.clock()
-                if (initialBuy == 0):
-                    initialBuy = cost
-                    initialBuyTs = currTs
-            elif action ==1:
-                if TradingEnabled:
-                    if order != None:
-                        if order['status']  != 'FILLED':
-                            sleep(0.1)
-                    orders = client.get_my_trades(symbol=sym,limit = 1)
-                    sell_price = orders[0]['price']
-                else:
-                    sell_price = price
+        #2 order depth
+        asks = []
+        bids = []
+        if 0:
+            depth = client.get_order_book(symbol=sym)
+            asks = depth['asks']
+            bids = depth['bids']
+        
+        #3. history for MACD, RSI
+        history.append(price)
+        if len(history) > MAX_HIST:
+            history = history[1:MAX_HIST+1]
+            per_min_history = get_per_min_history(history);
+            run = True
+        elif len(history)%(int(MAX_HIST/20)) == 0:
+            print (time_now() + str(int(len(history)/(int(MAX_HIST/20)))*5) + "% history filled")
 
-                if(float(sell_price) < 0.999 * float(price)):
-                    print("Trading Lantency cause > 0.1% lost! Sell Price: "+ str(sell_price)+ " Last Price: " + str(price))
+        if FastFill == True and len(history) == MAX_HIST:
+            samplingRate = originalSR
+            FastFill = False
+            LOG("FastFill Completed, now samping rate = " + str(samplingRate))
+            LOG("-----------------------------------------------------------")
 
-                profit = (float(sell_price) * (1-fee)-float(cost))/float(cost)
-                print("Last Sell Price: " + str(sell_price) + " Cost: " + str(cost) + " Profit%: " + str(profit*100) + " Time Taken: "+ str(currTs-lastSellTs) + " s")
+        if(run):
+            #Call HFT module to decide
+            currTs = time.clock()
+            action = HFT_algo.HFT(cost, lastBuyTs, currTs, price, fee, per_min_history, asks, bids, lastAction, useRSI)
 
-                accProfit += profit
-                profit_if_no_sell = (float(sell_price) * (1-fee) -float(initialBuy))/float(initialBuy)
-                print("Accumulated Profit: " + str(accProfit*100) + " Profit if no sell: " + str(profit_if_no_sell*100) + " Time Taken: "+ str(currTs-initialBuyTs) + " s")
-                lastSellTs = currTs                
+            #Perform action
+            if (action > 0 and action != lastAction):
+                order = Perform(action,price,amount)
+                lastAction = action
+            
+                if action == 2:
+                    if TradingEnabled:
+                        if order != None:
+                            if order['status']  != 'FILLED':
+                                sleep(0.1)
+                        orders = client.get_my_trades(symbol=sym,limit = 1)
+                        cost = orders[0]['price']
+                    else:
+                        cost = price
 
-    time.sleep(samplingRate)
+                    #add fees to price
+                    cost = str((1+fee) * float(cost))
+                    LOG("Bought@" + str(cost) +" x" + str(amount))
+                    lastBuyTs = time.clock()
+                    if (initialBuy == 0):
+                        initialBuy = cost
+                        initialBuyTs = currTs
+                elif action ==1:
+                    if TradingEnabled:
+                        if order != None:
+                            if order['status']  != 'FILLED':
+                                sleep(0.1)
+                        orders = client.get_my_trades(symbol=sym,limit = 1)
+                        sell_price = orders[0]['price']
+                    else:
+                        sell_price = price
 
+                    if(float(sell_price) < 0.999 * float(price)):
+                        LOG("Trading Lantency cause > 0.1% lost! Sell Price: "+ str(sell_price)+ " Last Price: " + str(price))
+
+                    profit = (float(sell_price) * (1-fee)-float(cost))/float(cost)
+                    LOG("Last Sell Price: " + str(sell_price) + " Cost: " + str(cost) + " Profit%: " + str(profit*100) + " Time Taken: "+ str(currTs-lastSellTs) + " s")
+
+                    accProfit += profit
+                    profit_if_no_sell = (float(sell_price) * (1-fee) -float(initialBuy))/float(initialBuy)
+                    LOG("Accumulated Profit: " + str(accProfit*100) + " Profit if no sell: " + str(profit_if_no_sell*100) + " Time Taken: "+ str(currTs-initialBuyTs) + " s")
+                    lastSellTs = currTs                
+
+        time.sleep(samplingRate)
+finally:
+    LOG("ENDING... Accumulated Profit: " + str(accProfit*100) + " Profit if no sell: " + str(profit_if_no_sell*100) + " Time Taken: "+ str(currTs-initialBuyTs) + " s")
     
 
